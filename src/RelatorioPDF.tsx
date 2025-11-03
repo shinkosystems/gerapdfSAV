289e3017eca45bf43b5efa237e69c0f404c4778c
Gustavo Costa Rodrigues <gustavocst98@gmail.com>
2025-11-03 17:18:16 -0300

    Títulos ajustados 02

--- src/RelatorioPDF.tsx
+++ src/RelatorioPDF.tsx
@@ -206,9 +206,9 @@ const CapaPDF = ({ data, nrsList }: { data: RelatorioData, nrsList: string[] }) 
                 <Text style={styles.coverDetail}>
                     <Text style={styles.coverDetailBold}>Inspeção finalizada em:</Text> {data.datafinalinspecao}
                 </Text>
-                {/* NOVO CAMPO: Responsável pelo relatório */}
+                {/* NOVO CAMPO: Responsável pelo projeto */}
                 <Text style={styles.coverDetail}>
-                    <Text style={styles.coverDetailBold}>Responsável pelo relatório:</Text> {data.responsavel}
+                    <Text style={styles.coverDetailBold}>Responsável pelo projeto:</Text> {data.responsavel}
                 </Text>
                 {nrsList.length > 0 && (
                     <Text style={{ ...styles.coverDetail, marginTop: 10 }}>
